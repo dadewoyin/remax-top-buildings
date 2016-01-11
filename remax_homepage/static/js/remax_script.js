a1a65cda033f32c6d7b3983d57648129426b8d70
EF <ef4166@gmail.com>
2016-01-11 13:35:16 -0500

    Allow coloring effect to work whether img or text is hovered

--- remax_homepage/static/js/remax_script.js
+++ remax_homepage/static/js/remax_script.js
@@ -1,7 +1,22 @@
+$(window).load(function () { 
+    $("#loader").fadeOut("slow");
+    $('body').css('padding-top', parseInt($('#pageHeader').css("height")));
+});
+
 $(window).resize(function () { 
     $('body').css('padding-top', parseInt($('#pageHeader').css("height")));
 });
 
-$(window).load(function () { 
-    $('body').css('padding-top', parseInt($('#pageHeader').css("height")));
+$(document).ready(function(){
+    //Apply coloring effect whether img or text is hovered over
+    $(".imgNeighborhoodName").hover(function(){
+        $(this).prev().css({
+            "-webkit-filter":"grayscale(0%)",
+            "filter":"grayscale(0%)"});
+        }, function(){
+        $(this).prev().css({
+            "-webkit-filter":"grayscale(100%)",
+            "filter":"grayscale(100%)"
+        });
+    });
 });
